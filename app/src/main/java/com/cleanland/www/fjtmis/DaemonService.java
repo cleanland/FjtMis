df6d5ab2265e65c26ce3a6a0f4e21b860a598c7b
Administrator <479949631@qq.com>
2014-10-24 20:16:49 +0800

    总算是恢复完了。。。。累个关死又。

--- app/src/main/java/com/cleanland/www/fjtmis/DaemonService.java
+++ app/src/main/java/com/cleanland/www/fjtmis/DaemonService.java
@@ -1,13 +1,19 @@
 package com.cleanland.www.fjtmis;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Vibrator;
+import android.util.Log;
 import android.widget.Toast;
 
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -40,15 +46,35 @@ public class DaemonService extends Service {
                             //振动提醒客户，3秒钟
                             ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(3000);
                         }
-                        lastResult=result;
+                        lastResult = result;
                     }
 
                     @Override
                     protected String doInBackground(Void... arg0) {
                         try {
-                            return CwyWebJSON.postToUrl(((MyApplication) getApplication()).getSiteUrl() + "/Office/GetBlogList", null);
+
+                            LinkedList params = new LinkedList<BasicNameValuePair>();
+
+                            SharedPreferences preferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+                            String account = preferences.getString("account", "");
+                            String pwd = preferences.getString("pwd", "");
+                            String siteurl = preferences.getString("siteUrl", "");
+                            Log.i("account", "" + account);//
+                            Log.i("pwd", "" + pwd);//
+                            Log.i("siteUrl", "" + siteurl);//
+                            if (siteurl.isEmpty()) return "";//如果是空值，是不会出错，也不会振动的，参见onPostExecute。。。
+
+                            params.add(new BasicNameValuePair("user", "" + account));
+                            params.add(new BasicNameValuePair("pwd", "" + pwd));
+
+                            CwyWebJSON.postToUrl(siteurl + "/home/logon", params);
+
+                            params.clear();
+                            return CwyWebJSON.postToUrl(siteurl + "/Office/GetBlogList", params);
+
                         } catch (Exception e) {
                             e.printStackTrace();
+                            Log.i("后台服务", "出错了。。。。。。");//
                         }
                         return "";
                     }
